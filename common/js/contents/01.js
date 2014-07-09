c90c5b0fb9d8cf1c8ab7ab530266ebef95ad6b94
0ness <nakamura@wonderful.co.jp>
2014-07-09 17:12:50 +0900

    no message

--- common/js/contents/01.js
+++ common/js/contents/01.js
@@ -38,26 +38,22 @@ function init(){
 		function DotLine(x01,y01,x02,y02,len,spd,size,color){
 
 			//座標系オブジェクト
-			this.zone = {
-				sx:x01,
-				sy:y01,
-				ex:x02,
-				ey:y02,
-				distance:0
-			};
+			this.sx = x01;
+			this.sy = y01;
+			this.ex = x02;
+			this.ey = y02;
+			this.distance = 0;
 
 			//Dotオブジェクト用オブジェクト
-			this.dot = {
-				axis:1,			//1：通常のxループ 2:反転のxループ 3:通常のyループ 4:反転のyループ
-				len:len+1,		//格納するオブジェクトの数
-				size:size,		//オブジェクトのサイズ
-				color:color,
-				spdX:0,
-				spdY:0,		//オブジェクトの速度
-				disX:0,	//オブジェクト毎の一定の距離
-				disY:0,	//オブジェクト毎の一定の距離
-				loop:spd*10000,
-			};
+			this.axis = 1;			//1：通常のxループ 2:反転のxループ 3:通常のyループ 4:反転のyループ
+			this.len = len+1;		//格納するオブジェクトの数
+			this.size = size;		//オブジェクトのサイズ
+			this.color = color || "#fff";
+			this.spdX = 0;
+			this.spdY = 0;		//オブジェクトの速度
+			this.disX = 0;	//オブジェクト毎の一定の距離
+			this.disY = 0;	//オブジェクト毎の一定の距離
+			this.loop = spd*10000;
 			this.arr = [];			//格納用配列
 			
 			aryLineObj.push(this);
@@ -66,71 +62,66 @@ function init(){
 		};		
 		DotLine.prototype = {
 			init:function(){
-				var that = this;
-				var zone = this.zone;
-				var dis_x = zone.ex - zone.sx;
-				var dis_y = zone.ey - zone.sy;
-				var dot = this.dot;
+				var dis_x = this.ex - this.sx;
+				var dis_y = this.ey - this.sy;
 
 				//1つずつの距離
-				dot.disX = (dis_x / (dot.len-1));
-				dot.disY = (dis_y / (dot.len-1));
+				this.disX = (dis_x / (this.len-1));
+				this.disY = (dis_y / (this.len-1));
 
 				//始点と終点の距離
-				zone.distance = abs(dis_x);
+				this.distance = abs(dis_x);
 
 				//移動スピード
-				dot.spdX = zone.distance / dot.loop * numTimeSpd;
-				dot.spdY = abs(dis_y) / dot.loop * numTimeSpd;
-				dot.spdX = (zone.sx > zone.ex) ? -dot.spdX : dot.spdX;
-				dot.spdY = (zone.sy > zone.ey) ? -dot.spdY : dot.spdY;
+				this.spdX = this.distance / this.loop * numTimeSpd;
+				this.spdY = abs(dis_y) / this.loop * numTimeSpd;
+				this.spdX = (this.sx > this.ex) ? -this.spdX : this.spdX;
+				this.spdY = (this.sy > this.ey) ? -this.spdY : this.spdY;
 
 				//オブジェクト生成・設定・格納
-				for(var i = 0; i<dot.len; i++){
+				for(var i = 0; i<this.len; i++){
 					var d = new Dot();
 					d.id = i;
-					d.size = dot.size;
-					d.originalSize = dot.size;
-					d.spdY = dot.spdY;
-					d.spdX = dot.spdX;
-					d.x = (zone.sx+i*dot.disX)-dot.disX;
-					d.y = (zone.sy+i*dot.disY)-dot.disY;
-					d.move = zone.ex - d.x;
+					d.size = this.size;
+					d.originalSize = this.size;
+					d.spdY = this.spdY;
+					d.spdX = this.spdX;
+					d.x = (this.sx+i*this.disX)-this.disX;
+					d.y = (this.sy+i*this.disY)-this.disY;
+					d.move = this.ex - d.x;
 					this.arr[i] = d;
 				}
 				return false;
 			},
 			arcDraw:function(){
 
-				var that = this;
 				var arr = this.arr;
-				var axis = that.dot.axis;
+				var axis = this.axis;
 				var dis = 0;
 				var flg = false;
 				var dot = this.dot;
-				var len = dot.len;
+				var len = this.len;
 				
 				for(var i=0; i<len; i++){
 
 					var d = arr[i];
-					dis = abs(d.x - that.zone.sx) - that.zone.distance;
+					dis = abs(d.x - this.sx) - this.distance;
 					flg = (dis > 0) ? true : false;
 
 					if(flg === true){
 						var prev = d.id+1;
-						if(prev === dot.len) prev = 0;
-						d.y = arr[prev].y-dot.disY;
-						d.x = arr[prev].x-dot.disX;
+						if(prev === this.len) prev = 0;
+						d.y = arr[prev].y-this.disY;
+						d.x = arr[prev].x-this.disX;
 					}
 
-					ctx.fillStyle = dot.color;
+					ctx.fillStyle = this.color;
 
 					//描画
 					d.draw();
 
 					d.x += d.spdX;
 					d.y += d.spdY;
-
 				}
 				return false;
 			},
@@ -186,7 +177,7 @@ function init(){
 			spdX:0,
 			spdY:0,
 			angle:(Math.random()*360)>>0,		// 角度（初期値）
-			change:0.5,	// 増やす角度（運動の増減値）
+			change:2,	// 増やす角度（運動の増減値）
 			radius:2,	// 半径（折り返す値）
 			radian:0,
 			by:0,
@@ -228,48 +219,48 @@ function init(){
 
 
 		//オブジェクト設定
-		var line_01 = new DotLine(numWidth,895,0,395,20,42,2,"#fff");
-		var line_02 = new DotLine(numWidth,960,0,440,20,20,1,"#fff");
-		var line_03 = new DotLine(numWidth,1200,0,600,10,4,1,"#fff");
-		var line_04 = new DotLine(numWidth,900,0,400,40,40,1,"#fff");
-		var line_05 = new DotLine(numWidth,1080,0,490,10,8,2,"#fff");
-		var line_06 = new DotLine(numWidth,1100,0,500,20,6,0.8,"#fff");
-		var line_07 = new DotLine(numWidth,1380,0,730,8,3,1.5,"#fff");
-		var line_08 = new DotLine(numWidth,1380,0,730,6,3,4,"#fff");
-		var line_09 = new DotLine(numWidth,900,0,415,30,30,0.5,"#fff");
-		var line_10 = new DotLine(numWidth,890,0,400,80,25,0.5,"#fff");
-		var line_11 = new DotLine(numWidth,960,0,400,40,15,0.5,"#fff");
+		var line_01 = new DotLine(numWidth,895,0,395,20,42,2);
+		var line_02 = new DotLine(numWidth,960,0,440,20,20,1);
+		var line_03 = new DotLine(numWidth,1200,0,600,10,4,1);
+		var line_04 = new DotLine(numWidth,900,0,400,40,40,1);
+		var line_05 = new DotLine(numWidth,1080,0,490,10,8,2);
+		var line_06 = new DotLine(numWidth,1100,0,500,20,6,0.8);
+		var line_07 = new DotLine(numWidth,1380,0,730,8,3,1.5);
+		var line_08 = new DotLine(numWidth,1380,0,730,6,3,4);
+		var line_09 = new DotLine(numWidth,900,0,415,30,30,0.5);
+		var line_10 = new DotLine(numWidth,890,0,400,80,25,0.5);
+		var line_11 = new DotLine(numWidth,960,0,400,40,15,0.5);
 		var line_12 = new DotLine(numWidth,878,0,388,4,50,2,"#ff0000");
-		var line_13 = new DotLine(numWidth,1380,0,700,3,3,10,"#fff");
-		var line_14 = new DotLine(numWidth,950,0,420,35,15,0.5,"#fff");
-		var line_15 = new DotLine(numWidth,1050,0,480,25,10,0.5,"#fff");
-		var line_16 = new DotLine(numWidth,1160,0,580,30,5,0.4,"#fff")
-		var line_17 = new DotLine(numWidth,1170,0,590,23,4,0.8,"#fff");
-		var line_18 = new DotLine(numWidth,1050,0,470,6,8,2,"#fff");
+		var line_13 = new DotLine(numWidth,1380,0,700,3,3,10);
+		var line_14 = new DotLine(numWidth,950,0,420,35,15,0.5);
+		var line_15 = new DotLine(numWidth,1050,0,480,25,10,0.5);
+		var line_16 = new DotLine(numWidth,1160,0,580,30,5,0.4)
+		var line_17 = new DotLine(numWidth,1170,0,590,23,4,0.8);
+		var line_18 = new DotLine(numWidth,1050,0,470,6,8,2);
 		var line_19 = new DotLine(numWidth,890,0,393,20,20,0.5,"#ff0000");
-		var line_20 = new DotLine(numWidth,895,0,415,8,30,1,"#fff");
-		var line_21 = new DotLine(numWidth,1430,0,700,8,2,2,"#fff");
-		var line_22 = new DotLine(numWidth,910,0,420,30,14,0.5,"#fff");
-		var line_23 = new DotLine(numWidth,1600,0,840,20,3,0.5,"#fff");
-		var line_24 = new DotLine(numWidth,1580,0,820,20,4,2,"#fff");
-		var line_25 = new DotLine(numWidth,910,0,405,20,10,0.5,"#fff");
-		var line_26 = new DotLine(numWidth,900,0,395,5,40,3,"#fff");
-		var line_27 = new DotLine(numWidth,1210,0,605,3,5,4,"#fff");
-		var line_28 = new DotLine(numWidth,1300,0,640,3,2,3,"#fff");
-		var line_29 = new DotLine(numWidth,1350,0,650,15,3,0.5,"#fff");
+		var line_20 = new DotLine(numWidth,895,0,415,8,30,1);
+		var line_21 = new DotLine(numWidth,1430,0,700,8,2,2);
+		var line_22 = new DotLine(numWidth,910,0,420,30,14,0.5);
+		var line_23 = new DotLine(numWidth,1600,0,840,20,3,0.5);
+		var line_24 = new DotLine(numWidth,1580,0,820,20,4,2);
+		var line_25 = new DotLine(numWidth,910,0,405,20,10,0.5);
+		var line_26 = new DotLine(numWidth,900,0,395,5,40,3);
+		var line_27 = new DotLine(numWidth,1210,0,605,3,5,4);
+		var line_28 = new DotLine(numWidth,1300,0,640,3,2,3);
+		var line_29 = new DotLine(numWidth,1350,0,650,15,3,0.5);
 		var line_30 = new DotLine(numWidth,891,0,394,20,20,0.5,"#ff0000");
-		var line_37 = new DotLine(numWidth,1020,0,460,3,8,3,"#fff");
+		var line_37 = new DotLine(numWidth,1020,0,460,3,8,3);
 		var line_38 = new DotLine(0,200,numWidth,300,3,10,1,"#ff0000");
 		var line_39 = new DotLine(numWidth,200,0,300,3,10,1,"#ff0000");
-		var line_40 = new DotLine(numWidth,200,0,320,8,15,0.5,"#fff");
-		var line_41 = new DotLine(numWidth,940,0,420,40,10,1,"#fff");
+		var line_40 = new DotLine(numWidth,200,0,320,8,15,0.5);
+		var line_41 = new DotLine(numWidth,940,0,420,40,10,1);
 		//部分ループ
-		var line_31 = new DotLine(1000,652,300,473,20,25,1,"#fff");
-		var line_32 = new DotLine(1000,657,300,478,30,25,0.5,"#fff");
+		var line_31 = new DotLine(1000,652,300,473,20,25,1);
+		var line_32 = new DotLine(1000,657,300,478,30,25,0.5);
 		var line_33 = new DotLine(800,535,930,0,8,15,0.8,"#ff0000");
-		var line_34 = new DotLine(820,450,930,0,3,5,0.5,"#fff");
-		var line_35 = new DotLine(1200,700,200,449,30,15,0.5,"#fff");
-		var line_36 = new DotLine(1200,720,200,469,40,10,0.4,"#fff");
+		var line_34 = new DotLine(820,450,930,0,3,5,0.5);
+		var line_35 = new DotLine(1200,700,200,449,30,15,0.5);
+		var line_36 = new DotLine(1200,720,200,469,40,10,0.4);
 
 		
 		//オブジェクト生成
@@ -287,7 +278,7 @@ function init(){
 			return false;
 		};
 
-			win.requestAnimFrame(loopAnim);
+		win.requestAnimFrame(loopAnim);
 
 		return false;
 	}();
